9ffbe0638f1744c6fa4b960ddf5cc1eba49eab01
jesus <jesus@LAPTOP-JT049GVH>
2024-10-04 13:47:45 +0530

    pushing framework

---
+++ vamsiFramework/src/test/java/ContactTest/ToCreateContact.java
@@ -0,0 +1,10 @@
+package ContactTest;
+
+public class ToCreateContact {
+
+	public static void main(String[] args) {
+		System.out.println("Create contact successfully");
+
+	}
+
+}
---
+++ vamsiFramework/src/test/java/OrganizationTest/ToCreateOrganization.java
@@ -0,0 +1,10 @@
+package OrganizationTest;
+
+public class ToCreateOrganization {
+
+	public static void main(String[] args) {
+		System.out.println("create organization successfully");
+
+	}
+
+}
